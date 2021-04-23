449eb3ae292b26ba056413df4352df9367715249
PuntitOwO <christopher.marin@ug.uchile.cl>
2021-04-23 00:23:19 -0400

    demo build uwu

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -13,18 +13,24 @@ var tpcamera = false #false: FP, true: TP
 var changecamerakey = KEY_F
 onready var fpc = get_node("Gimbal_h_cam_FP/Gimbal_v_cam/FP Camera")
 onready var tpc = get_node("Gimbal_h_cam_TP/Gimbal_v_cam/TP Camera")
+onready var h_node = get_node("Gimbal_h_cam_FP")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	fpc.make_current()
+
 func _unhandled_key_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.scancode == changecamerakey:
 			if tpcamera:
 				tpcamera = false
 				fpc.make_current()
+				h_node = get_node("Gimbal_h_cam_FP")
 			else:
 				tpcamera = true
 				tpc.make_current()
+				h_node = get_node("Gimbal_h_cam_TP")
+		elif event.pressed and event.scancode == KEY_R:
+			get_tree().reload_current_scene()
 
 
 func _physics_process(delta):
@@ -35,7 +41,7 @@ func _physics_process(delta):
 	
 	var dir_z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	var dir_x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var horizontal_vel = transform.basis.x * dir_x + transform.basis.z * dir_z
+	var horizontal_vel = h_node.transform.basis.x * dir_x + h_node.transform.basis.z * dir_z
 	var vertical_vel = Vector3(0, 0, 0)
 	
 	if Input.is_action_just_pressed("ui_jump") and on_floor:
