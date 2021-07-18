5bfa815fdb3c30ce0ef8d574d8e81f177892fea5
PuntitOwO <christopher.marin@ug.uchile.cl>
2021-07-18 17:26:14 -0400

    Credits animation finished :D

--- scripts/Managers/SpaceBody.gd
+++ scripts/Managers/SpaceBody.gd
@@ -25,7 +25,22 @@ func _on_LoadGame_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_Credits_pressed() -> void:
+	# Transición a créditos
 	$Camera/AnimationPlayer.play("CameraCredits", -1, 0.5)
+	yield(get_tree().create_timer(2), "timeout")
+	$AnimationPlayer.play("Credits")
+	yield($Camera/AnimationPlayer, "animation_finished")
+	# En créditos
+	$CanvasLayer/Tween.interpolate_property($CanvasLayer/CreditsUI, "rect_position", Vector2(0,0), Vector2(0,-1000), 9.5, Tween.TRANS_LINEAR)
+	$CanvasLayer/Tween.start()
+	yield(get_tree().create_timer(10), "timeout")
+	# Vuelta a menú principal
+	$Camera/AnimationPlayer.play("CameraCredits", -1, -0.5, true)
+	yield(get_tree().create_timer(1.5), "timeout")
+	$AnimationPlayer.play("Credits", -1, -1.0, true)
+	yield($Camera/AnimationPlayer, "animation_finished")
+	$CanvasLayer/CreditsUI.rect_position.y = 0
+	get_node("CanvasLayer/MainMenuGUI/VBoxContainer/Button/NewGame").grab_focus()
 
 func _on_Exit_pressed() -> void:
 	LevelManager.Game.fade.fade_in()
