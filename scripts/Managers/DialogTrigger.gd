d8548c821e8d070692bca9d5517cc3a6054c528f
PuntitOwO <christopher.marin@ug.uchile.cl>
2021-07-23 22:26:57 -0400

    gravitometer temporal toggle script merged to dialog trigger

--- scripts/Managers/DialogTrigger.gd
+++ scripts/Managers/DialogTrigger.gd
@@ -4,6 +4,8 @@ export var dialog_mode = "AUTO"
 export var one_shot    = true
 export (Array, String) var dialogues
 
+export var activate_particles = false
+
 onready var collision  = $CollisionShape
 
 func _ready() -> void:
@@ -11,5 +13,7 @@ func _ready() -> void:
 
 func _on_body_entered(body: Spatial):
 	if body.is_in_group("Player"):
+		if activate_particles:
+			body.temporal_gravitometer(3)
 		body.comment(dialogues, dialog_mode)
 		collision.disabled = one_shot
