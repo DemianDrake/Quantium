12cc6dad1fc40917df55524d7790fd967d078ace
DemianDrake <damian.ibarra@ug.uchile.cl>
2021-07-29 01:49:14 -0400

    Outro Update
    Added outro cutscene

--- scripts/Managers/CutsceneManager.gd
+++ scripts/Managers/CutsceneManager.gd
@@ -9,22 +9,45 @@ onready var location_2 = $"Location Margin/Location 02"
 onready var text = $"Text Boxes"
 onready var logo = $GameLogo
 onready var color_layer = $"Color Layer"
+onready var outro = $"Outro Text"
 
-export var current = 0
-var scene = "Scene 0"
+export var current = -1
+var scene = "Scene "
 
+func _ready():
+	# current = LevelManager.Game.current_cutscene
+	current = 6
+	anim_tree.stop()
+
+func set_text():
+	match current:
+		8:
+			outro.text = "With C.R.O.N.O.S. repaired, the Quantium stabilized"
+		9:
+			outro.text = "I started looking for the rest of the team. When I found them, I was glad they were lucky enough to be unharmed"
+		10:
+			outro.text = "They were scattered all over the place, so it took a while to find all of them"
+		11:
+			outro.text = "But with everyone's help we managed to repair the damages caused by the incident, so that this incident would never happen again"
+		12:
+			outro.text = "Or so we thought"
 
 func playscene():
 	background.texture.set_current_frame(current - 1)
-	scene = "Scene 0%d" % current
+	scene = "Scene %d" % current
+	print(scene)
 	anim_tree.travel(scene)
 	if current == 5:
 		alert_anim.play("Alert")
+	elif current >= 8:
+		set_text()
 	current += 1
 
 func _process(_delta):
 	if current == -1:
 		return
+	if current == 6 or current == 0:
+		anim_tree.start("Start")
 	if anim_tree.get_current_node() == "Start":
 		anim_tree.stop()
 		playscene()
@@ -33,4 +56,7 @@ func _process(_delta):
 	elif anim_tree.get_current_node() == "Ended":
 		LevelManager.change_scene("res://scenes/rooms/tutorial/tutorial.tscn")
 		LevelManager.Game.music_fade_and_change(2, 1)
+		current = -1
+	elif anim_tree.get_current_node() == "Back to Main Menu":
+		LevelManager.change_scene("res://scenes/Main.tscn")
 		current = -1
